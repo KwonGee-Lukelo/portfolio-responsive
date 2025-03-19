cc47ac5144bbae2248dbd0618e3c90886426d357
KwonGee-Lukelo <wahabfall17@gmail.com>
2025-03-19 15:17:54 +0100

    Création dune fonction qui permet de fermer le menu si on clique sur n'importe quel lien de navigation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
+const header = document.querySelector(".header");
 const btnMenu = document.querySelector("#nav-toggle");
 const btnClose = document.querySelector("#nav-close");
+const navLink = document.querySelectorAll(".nav__link");
 
 //Evenement
 btnMenu.addEventListener("click", menu);
@@ -8,6 +10,7 @@ btnClose.addEventListener("click", close);
 //Function pour ouvrir menu de navigation (Hamburger) sur petit écran
 function menu(e) {
   e.preventDefault();
+
   document.querySelector(".nav__list").classList.add("nav__list__mobile");
   document.querySelector(".nav__close").classList.add("nav__close__mobile");
   console.log("Yes!!!");
@@ -18,3 +21,11 @@ function close() {
   document.querySelector(".nav__list").classList.remove("nav__list__mobile");
   document.querySelector(".nav__close").classList.remove("nav__close__mobile");
 }
+
+/*Cette fonction permet de fermer le menu si on clique 
+à n'importe qu'elle lien de navigation*/
+const linkAction = () => {
+  document.querySelector(".nav__list").classList.remove("nav__list__mobile");
+  document.querySelector(".nav__close").classList.remove("nav__close__mobile");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
