bde3cecb05c09c14466c6b1345b655829ede5fb9
KwonGee-Lukelo <wahabfall17@gmail.com>
2025-03-22 16:32:38 +0100

    On gère la validation du formulaire

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,14 @@
 const btnMenu = document.querySelector("#nav-toggle");
 const btnClose = document.querySelector("#nav-close");
 const navLink = document.querySelectorAll(".nav__link");
+const form = document.querySelector("#form");
+const btnForm = document.querySelector("#submit");
 
 //Evenement
 btnMenu.addEventListener("click", menu);
 btnClose.addEventListener("click", close);
 window.addEventListener("scroll", shadowHeader);
+form.addEventListener("submit", validateForm);
 
 //Function pour ouvrir menu de navigation (Hamburger) sur petit écran
 function menu(e) {
@@ -40,4 +43,42 @@ function shadowHeader() {
   this.scrollY >= 50
     ? header.classList.add("shadow__header")
     : header.classList.remove("shadow__header");
+}
+
+// Fonction qui nous permet de validé le formulaire
+function validateForm(e) {
+  e.preventDefault();
+
+  //On accède à tout les elements du formulaires
+  const formElement = e.target.elements;
+
+  //On recupere le valeur des champs de saisies
+  const nom = formElement.nom.value;
+  const prenom = formElement.prenom.value;
+  const email = formElement.email.value;
+  const sujet = formElement.sujet.value;
+  const message = formElement.message.value;
+  const gmail = /^[a-zA-Z0-9._%+-]+@gmail\.com$/; //on definit le format de l'adresse mail
+
+  //On test que tout le champ de saisi ne soit pas vide
+  if (
+    nom !== "" &&
+    prenom !== "" &&
+    email !== "" &&
+    sujet !== "" &&
+    message !== ""
+  ) {
+    //On verifie si le format d'un adresse mail est respecté
+    if (!gmail.test(email)) {
+      alert(
+        "Vous devez respecté le format d'une adresse mail (ex: pkwongee@gmail.com)"
+      );
+      return;
+    }
+    alert("Votre message a été envoyer avec success!");
+    return;
+  } else {
+    alert("Veuillez remplir tout les champs correctement");
+    return;
+  }
 }
